4b0e318b4e72b1a318b880d664e4bdf4eb326170
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-08 05:06:12 +0800

    updated add and show in main

--- Project01_ToDoList/main.py
+++ Project01_ToDoList/main.py
@@ -1,19 +1,25 @@
-todos = []
-
-todos = []
-
-
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ").strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
             todos.append(todo)
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 item = item.title()
                 print(f"{index + 1}-{item}")
+
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
