af4f63b2fb2b0f127cbbf1290af0ebdd6ddbafcd
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2022-12-26 23:20:37 +0800

    added default argument to functions

--- Project01_ToDoList/main.py
+++ Project01_ToDoList/main.py
@@ -1,9 +1,9 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
     with open(filepath, "r") as file_local:
             todos_local = file_local.readlines()  
     return todos_local 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
@@ -15,15 +15,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:] + "\n"
 
-        todos = get_todos("todos.txt") 
+        todos = get_todos() 
 
         todos.append(todo)
         
-        write_todos('todos.txt', todos)
+        write_todos(todos)
         
     elif user_action.startswith('show'):
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.title().strip('\n')
@@ -34,30 +34,29 @@ while True:
             number = int(user_action[5:])
             number = number - 1
             
-            todos = get_todos("todos.txt")
+            todos = get_todos()
                 
             new_todo = input("Add your new todo: ")
             todos[number] = new_todo + "\n"
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
             continue
 
 
-
     elif user_action.startswith('complete'):
         try:
             number = int(user_action[9:])
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')         
             todos.pop(index)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
